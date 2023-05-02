f6be5491ac6b986d07c50fc85daa2fd6d1de4998
adrien <adrien.no@proton.me>
2023-05-02 23:43:52 +0200

    serie de if/else pour la coupure, on va plutot faire des if successifs enlevant des coups de la liste des coups, puis au final lancer la liste de coups

--- 2022/j19.ml
+++ 2022/j19.ml
@@ -8,7 +8,7 @@ type blueprint = {
 type ressources = Ore | Clay | Obsi | Geode
 type choix = Construire of ressources | Economiser
 
-let minutes_max = 23
+let minutes_max = 17
 (* ================================================================ entrée ================================================================ *)
 let data =
   let f = Scanf.Scanning.from_channel (open_in "j19.txt") in
@@ -63,56 +63,34 @@ let max_geode (bp:blueprint) : int =
       else if minute = minutes_max then
         n_geode
       else
+        (*on va faire une série de tests pour supprimer des branches.*)
 
-        (* (Printf.printf "\n=== Minute %i ===\n" (minute+1); *)
-        (* print_inv inv; *)
-        (* print_base base;) *)
-
-        (* on essaie de réduire les appels en donnant les meilleurs certains cas *)
-
-        if can_craft inv bp Geode then
-          (*Printf.printf "\n\n[ On rajoute un constructeur de geodes ]\n\n";*)
+        (* on peut toujours faire un constructeur de geodes *)
+        if r_ore >= fst bp.cgeode && r_obsi >= snd bp.cgeode then
           aux_naive inv base (minute+1) (Construire Geode)
 
-        (* il y a suffisamment d'un des matériaux mais pas assez de l'autre *)
+        (* on a assez d'obsidienne => il manque de l'ore *)
+        else if r_obsi >= snd bp.cgeode then
+          (* /\ améliorable (conséquences sur les coups suivants si on skip ou non) *)
+          max (aux_naive inv base (minute+1) (Economiser)) (aux_naive inv base (minute+1) (Construire Ore))
+        else
+          (* Il nous manque des obsis ou/et des ore *)
 
-        else if n_obsi >= snd bp.cgeode then
-          (* manque ore *)
-          aux_naive inv base (minute+1) (Construire Ore)
-        else if n_ore >= fst bp.cgeode then (
-          (* manque obsi *)
-          if can_craft inv bp Obsi then aux_naive inv base (minute+1) (Construire Obsi)
+          (* /\ peut-etre faux /\ on peut toujours créer un constructeur d'obsidiennes *)
+          if r_clay >= bp.cclay then
+            (* on a qu'à attendre pour l'obsi (on peut pas faire mieux) *)
+            (* on Eco pas car au pire on peut faire un constructeur d'obsi () *)
+            [Construire Obsi ; Construire Ore ; Construire Geode] |> List.map (fun action -> aux_naive inv base (minute+1) action) |> List.fold_left max 0
+
+
+      (* on a assez de core *)
+      else if r_ore >= fst bp.cgeode && r_ore >= bp.cclay && r_ore >= fst bp.cobsi then
+
+
           else
-            (* il faut constuire une obsidienne *)
-            if n_clay >= snd bp.cobsi then
-              (* manque ore *)
-               aux_naive inv base (minute+1) (Construire Ore)
-            else if n_ore >= fst bp.cobsi then
-              (* manque clay *)
-              aux_naive inv base (minute+1) (Construire Clay)
-            else
-              (* il manque des deux *)
-              [Construire Clay ; Construire Ore]
-              |> List.map (fun action -> aux_naive inv base (minute+1) action)
-              |> List.fold_left max 0
-          )
-        (* else if can_craft inv bp Obsi then ( *)
-        (*   Printf.printf "can craft Obsi\n"; *)
-        (*   aux_naive inv base (minute+1) (Construire Obsi)) *)
-        (* else if can_craft inv bp Clay then ( *)
-        (*   Printf.printf "can craft Clay\n"; *)
-        (*   aux_naive inv base (minute+1) (Construire Clay)) *)
-        (* else if can_craft inv bp Ore then ( *)
-        (*   Printf.printf "can craft Ore\n"; *)
-        (*   aux_naive inv base (minute+1) (Construire Ore)) *)
-        else if can_craft inv bp Obsi || can_craft inv bp Clay || can_craft inv bp Ore then
-          [Construire Clay ; Construire Obsi ; Construire Ore]
+          [Economiser ; Construire Clay ; Construire Obsi ; Construire Ore ; Construire Geode]
           |> List.map (fun action -> aux_naive inv base (minute+1) action)
           |> List.fold_left max 0
-
-        else
-          aux_naive inv base (minute+1) Economiser
-
 
   in
   (* le compteur "minute" est décalé de "un" par rapport à l'énoncé car nous on commence avec 1 ore par le robot (pour cette implémentation) *)
