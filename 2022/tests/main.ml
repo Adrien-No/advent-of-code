7a7f7303ce8e3f9376d8847c2b7599ce2f109ca6
adrien <adrien.no@proton.me>
2023-05-14 00:41:04 +0200

    finish

--- 2022/tests/main.ml
+++
@@ -1,6 +0,0 @@
-let data =
-  let f = Scanf.Scanning.from_channel open_in "file.txt" in
-  let rec read l =
-    try
-      read (Scanf.bscanf "Valeur %i : %i" (fun x,y -> (x,y)::l))
-    with End_of_file -> l
